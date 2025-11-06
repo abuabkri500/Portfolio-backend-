dd5212b4b62993d5a4ad153e1b4a5131be788cb0
Adebayo Abubakri <adebayoabubakriolakayode12345@gmail.com>
2025-11-06 17:49:19 +0100

    mycodes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: process.env.DEPLOYED_URL || "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const app = require("./app.js");
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${process.env.DEPLOYED_URL || `http://localhost:${port}`}`);
 });
